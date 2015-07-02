3349b2041cf4a9b58177e7a5fa4743efd40e405c
Jan Rembold <jan.rembold@virtual-identity.com>
2015-07-02 17:29:00 +0200

    complete refactoring to handle different elements in a single timeout loop

--- js/jquery.debouncedwidth.js
+++ js/jquery.debouncedwidth.js
@@ -1,6 +1,6 @@
 /*!
  *
- * jquery-debouncedwidth
+ * jquery-debouncedwidth - v1.1.0
  * https://github.com/janrembold/jquery-debouncedwidth
  * Copyright (c) 2015 Jan Rembold <janrembold@gmail.com>; License: MIT
  *
@@ -9,55 +9,63 @@
 (function($) {
     'use strict';
 
-    var $event = $.event,
-        $special,
-        resizeTimeout,
-        lastWidth;
+    var timeout;
+    var $window = $(window);
+    var lastWidth = $window.width();
+    var elements = [];
 
-    $special = $event.special.debouncedwidth = {
+    // use timeouts to debounce resize event
+    var debouncer = function(){
+        clearTimeout(timeout);
+        timeout = setTimeout( function(){
 
-        setup: function() {
-            // save last viewport width
-            lastWidth = $special.width();
+            // check if width really changed
+            var currentWidth = $window.width();
+            if(currentWidth !== lastWidth) {
+                // set current width to last seen width
+                lastWidth = currentWidth;
 
-            // attach resize handler
-            $(this).on('resize', $special.handler);
-        },
-
-        teardown: function() {
-            // remove resize handler
-            $(this).off('resize');
-        },
-
-        handler: function(event) {
-            // save context and arguments
-            var context = this,
-                args = arguments;
-
-            // clear and re-set resize timeout
-            clearTimeout(resizeTimeout);
-            resizeTimeout = setTimeout( function(){
-                $special.dispatcher(event, context, args);
-            }, $special.threshold);
-        },
-
-        dispatcher: function(event, context, args){
-            var newWidth = $special.width();
-            if(newWidth !== lastWidth) {
-                lastWidth = newWidth;
-
-                // set correct event type
-                event.type = 'debouncedwidth';
-                $event.dispatch.apply( context, args );
+                // trigger debouncedwidth event
+                var index = elements.length;
+                while(index--) {
+                    $(elements[index]).trigger('debouncedwidth');
+                }
             }
+
+        }, $.event.special.debouncedwidth.threshold);
+    };
+
+    var inArray = function(element){
+        var index = elements.length;
+        while(index--) {
+            if(elements[index] === element) {
+                return index;
+            }
+        }
+
+        return -1;
+    };
+
+    $.event.special.debouncedwidth = {
+        setup: function(){
+            if(inArray(this) === -1) {
+                elements.push(this);
+            }
+
+            var $this = $(this);
+            $this.on('resize.debouncedwidth', debouncer);
         },
 
-        width: function(){
-            return window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+        teardown: function(){
+            var index = inArray(this);
+            if(index !== -1) {
+                elements = elements.splice(index, 1);
+            }
+
+            $(this).off('resize.debouncedwidth');
         },
 
         threshold: 150
-
     };
 
 })(window.jQuery);
